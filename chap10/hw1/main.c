515d9b469b0e1da69075de73dd90798814b19db0
gyuri <gb58252@gmail.com>
2024-11-25 18:06:47 +0000

    Chap10

--- chap10/hw1/main.c
+++ chap10/hw1/main.c
@@ -1,20 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct node {
 	int data;
 	struct node *next;
 };
 
-void push(struct node *top, int data);
-int pop(struct node *top);
+void push(struct node **top, int data){
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	if(newNode == NULL) {
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
+	newNode->data = data;
+	newNode->next = *top;
+	*top = newNode;
+}
+
+int pop(struct node **top){
+	if(*top == NULL) {
+		printf("Stack is empty.\n");
+		return -1;
+	}
+	struct node *temp = *top;
+	int popValue = temp->data;
+	*top = temp->next;
+	free(temp);
+	return popValue;
+}
 
 int main() {
-	
-	scanf("%d"); 
+	struct node *stack = NULL;
+		int value;
 
-	printf("Print stack\n");
-	printf("%d\n");
+	while(1) {
+		if(scanf("%d", &value) == 1){
+			push(&stack, value);
+		}
+		else {
+			printf("Print stack\n");
 
+			while(stack != NULL) {
+				printf("%d\n", pop(&stack));
+			}
+			break;
+		}
+	}
+	return 0;
 }
--- chap10/hw2/main.c
+++ chap10/hw2/main.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 struct node {
 	int data;
 	struct node *next;
 };
-void addq(struct node *head, struct node *tail, int data);
-int delete(struct node *head, struct node *tail);
+
+void addq(struct node **head, struct node **tail, int data) {
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	if(newNode == NULL) {
+		printf("Memory allocation failed.\n");
+		exit(1);
+	}
+	newNode->data = data;
+	newNode->next = NULL;
+
+	if(*tail == NULL) *head = *tail = newNode;
+	else {
+		(*tail)->next = newNode;
+		*tail = newNode;
+	}
+}
+
+int delete(struct node **head, struct node **tail) {
+	if(*head == NULL) {
+		printf("Queue is Enpty.\n");
+		return -1;
+	}
+
+	struct node *temp = *head;
+	int deleteValue = temp->data;
+
+	*head = (*head)->next;
+	if(*head == NULL) *tail = NULL;
+
+	free(temp);
+	return deleteValue;
+}
 
 int main () {
+
+	struct node *head = NULL;
+	struct node *tail = NULL;
 	int num[200];
-	struct node *next, *head, *tail = NULL;
+	int count = 0;
+	int value;
 
-	scanf("%d ");
+	while(1) {
+		if(scanf("%d", &value) == 1) {
+			if(count < 200) {
+				num[count++] = value;
+				addq(&head, &tail, value);
+			}
+			else {
+				printf("Maximum input reached (200).\n");
+				break;
+			}
+		}
+		else {
+			printf("Print queue\n");
 
-	printf("Print queue\n");
-	
-	printf("%d\n");
-
+			while(head != NULL) {
+				printf("%d\n", delete(&head, &tail));
+			}
+			break;
+		}
+	}
+	return 0;
 }
